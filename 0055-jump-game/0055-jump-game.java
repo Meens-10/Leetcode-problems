e671b723ee368f6c419bf6ddb1ad175e9e7f2aff
Meens-10 <717823d134@kce.ac.in>
2026-03-01 20:59:51 +0530

    Time: 2 ms (85.8%), Space: 48 MB (42.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int max = 0;
-        for(int i = 0 ; i <= nums.length; i++){
-            if(i > max){
+        int maxreach = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            if(i > maxreach){
                 return false;
             }
-            max = Math.max(max,i+nums[i]);
+            maxreach = Math.max(maxreach,i+nums[i]);
         }
         return true;
     }
