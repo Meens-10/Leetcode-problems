54a874a6dce250be5a8c59197f4c8eaf08be088a
Meens-10 <717823d134@kce.ac.in>
2026-03-27 09:38:50 +0530

    Time: 1 ms (99.92%), Space: 48.7 MB (91.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+    if(nums2.length < nums1.length ){
+        return findMedianSortedArrays(nums2, nums1);
+         } 
+         int low = 0;
+         int high = nums1.length;
+       int m1;
+       int m2;
+       while(low <= high){
+       m1 = (low + high)/2;
+       m2 = ((nums1.length + nums2.length)+1)/2 - m1;
+       int l1 = (m1==0) ? Integer.MIN_VALUE : nums1[m1-1];
+int r1 = (m1==nums1.length) ? Integer.MAX_VALUE : nums1[m1];
 
-        int len1 = nums1.length, len2 = nums2.length;
-        int left = 0, right = len1;
+int l2 = (m2==0) ? Integer.MIN_VALUE : nums2[m2-1];
+int r2 = (m2==nums2.length) ? Integer.MAX_VALUE : nums2[m2];
+       if(l1<= r2 && l2<=r1){
+        if((nums1.length + nums2.length)%2==0){
+            return (Math.max(l1,l2) + Math.min(r1,r2)) / 2.0;
+        }else{
+            return (double) Math.max(l1,l2);
+         }
+       }
+         else if(l1>r2){
+            high = m1 - 1;
 
-        while (left <= right) {
-            int part1 = (left + right) / 2;
-            int part2 = (len1 + len2 + 1) / 2 - part1;
-
-            int maxLeft1 = (part1 == 0) ? Integer.MIN_VALUE : nums1[part1 - 1];
-            int minRight1 = (part1 == len1) ? Integer.MAX_VALUE : nums1[part1];
-            int maxLeft2 = (part2 == 0) ? Integer.MIN_VALUE : nums2[part2 - 1];
-            int minRight2 = (part2 == len2) ? Integer.MAX_VALUE : nums2[part2];
-
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return (double) Math.max(maxLeft1, maxLeft2);
-                }
-            } else if (maxLeft1 > minRight2) {
-                right = part1 - 1;
-            } else {
-                left = part1 + 1;
-            }
-        }
-        return 0.0;        
+         }else{
+            low = m1 + 1;
+         }
+       }
+        return 0.0;
     }
 }
